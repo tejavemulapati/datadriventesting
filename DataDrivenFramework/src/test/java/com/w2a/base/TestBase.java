85c186ea7ff7eb2901a494d31559a6de6603d3fd
tejav <tejav@DESKTOP-B5M7SCF>
2019-03-04 10:51:37 +0530

    v1

---
+++ DataDrivenFramework/src/test/java/com/w2a/base/TestBase.java
@@ -0,0 +1,29 @@
+package com.w2a.base;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/*All initializing will be done through this
+ * WebDriver
+ * Properties
+ * Logs
+ * ExtentReports
+ * DB
+ * Excel
+ * Mail
+ */
+
+//@BeforeSuite
+public class TestBase {
+	
+	@BeforeMethod
+	public void setUp(){
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		
+	}
+
+}
